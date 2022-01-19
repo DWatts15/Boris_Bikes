b1eaea51d471c94eb8e0903a5515984e9dc83109
Daniel Watts <d.watts15@outlook.com>
2022-01-19 19:00:49 +0000

    Challenge 11 completed. Added function + test for docking bike and storing bikes (attr reader).

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,22 @@ require "bike"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to :bike }
 
-  it "release a working bike" do
+  it "can release a working bike" do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it "can store a docked bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
